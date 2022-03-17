dde005fda689ea65958707e5316e3a88e57d3cd6
fujimonnmonn <ec2-user@ip-172-31-23-48.ec2.internal>
2022-03-17 17:05:45 +0000

    topページ用のhtml/css追加 リセットcss(ress.css)を初めに読むように追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   root to: 'homes#top'
-  #test
+  get '*unmatched_route' => redirect('/')
 end
